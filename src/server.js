de0b341e6897077e6dd803e64ba3ec6c4f5aec5d
Guilherme Betsa <ybtyza@gmail.com>
2024-07-25 15:25:07 -0300

    feat: 🎸 (Estrutura) Estrutura do servidor

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes/routes')
+const connection = require('./database/connection')
+
+const { APP_PORT } = process.env
+
+class Server{
+    constructor(server = express()){
+        this.middleware(server),
+        this.database(),
+        server.use(routes),
+        this.initializeServer(server)
+    }
+
+    async middleware(server){
+        console.log('Executando middleware.')
+        server.use(cors())
+        server.use(express.json())
+        console.log('Middleware executado com sucesso.')
+    }
+
+    async database(){
+        try {
+            console.log('Conectando ao banco de dados.')
+            await connection.authenticate()
+            console.log('Banco de dados conectado com sucesso.')
+        } catch {
+            console.log('Erro ao conectar ao banco de dados.')
+        }
+    }
+
+    async initializeServer(server){
+        server.listen(APP_PORT, () => {
+            console.log(`Servidor iniciado na porta ${APP_PORT}.`)
+        })
+    }
+}
+
+module.exports = { Server }

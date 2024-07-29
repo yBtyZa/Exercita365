34d5a698b8b2f4d7931b75832853ca9bbaa60f10
Guilherme Betsa <ybtyza@gmail.com>
2024-07-29 18:28:00 -0300

    feat: 🎸 (GoogleMaps) Rota para exibir o link do google maps

--- src/routes/locais.routes.js
+++ src/routes/locais.routes.js
@@ -8,5 +8,6 @@ locaisRoutes.get('/', LocaisController.listar)
 locaisRoutes.get('/:local_id', LocaisController.listarUm)
 locaisRoutes.delete('/:local_id', LocaisController.deletar)
 locaisRoutes.put('/:local_id', LocaisController.editar)
+locaisRoutes.get('/:local_id/maps', LocaisController.exibirLinkMaps)
 
 module.exports = locaisRoutes

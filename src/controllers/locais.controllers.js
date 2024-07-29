7e1428189ef73c774d7e244ad71252fc45075e1d
Guilherme Betsa <ybtyza@gmail.com>
2024-07-29 17:35:17 -0300

    feat: 🎸 (Update) Rota para atualizar locais

--- src/controllers/locais.controllers.js
+++ src/controllers/locais.controllers.js
@@ -19,7 +19,9 @@ class LocaisController {
                     }
                 }
                 )
-                if(atividades_id.length !== dados.atividades_id.length) {
+                const idsExistentes = atividades_id.map(a => a.id)
+                const idsNaoExistentes = dados.atividades_id.filter(id => !idsExistentes.includes(id))
+                if (idsNaoExistentes.length > 0) {
                     return res.status(400).json({ message: 'Uma ou mais atividades não existem!' })
                 }
                 const localidade = await buscarCep(dados.cep)
@@ -36,11 +38,12 @@ class LocaisController {
                 await novoLocal.addAtividades(atividades_id)
                 return res.status(201).json(dadosLocal);
             }
+            return res.status(400).json({ message: 'Selecione pelo menos uma atividade!' })
         } catch (error) {
             if (error.name === 'SequelizeUniqueConstraintError') {
                 return res.status(400).json({ message: 'Ja existe um local com esse nome!' })
             }
-            if(error.name === 'Error') {
+            if (error.name === 'Error') {
                 return res.status(400).json({ message: 'CEP inválido ou não encontrado!' })
             }
             return res.status(500).json({ message: 'Não foi possível criar o local' })
@@ -54,7 +57,7 @@ class LocaisController {
                     usuario_id: req.usuario.id
                 }
             })
-            if(locais.length === 0) {
+            if (locais.length === 0) {
                 return res.status(404).json({ message: 'Nenhum local encontrado!' })
             }
             return res.status(200).json(locais.map((local) => {
@@ -89,7 +92,7 @@ class LocaisController {
                     }
                 }
             })
-            if(!local) {
+            if (!local) {
                 return res.status(404).json({ message: 'Local não encontrado!' })
             }
             const atividades = local.atividades.map(atividade => ({
@@ -120,16 +123,76 @@ class LocaisController {
                     usuario_id: req.usuario.id
                 }
             })
-            if(!local) {
+            if (!local) {
                 return res.status(404).json({ message: 'Local não encontrado!' })
             }
             await LocaisAtividades.destroy({
                 where: { local_id: local_id }
-              });
+            });
             await local.destroy()
             return res.status(200).json({ message: 'Local excluído com sucesso!' })
         } catch (error) {
             return res.status(500).json({ message: 'Não foi possível deletar o local' })
+        }
+    }
+
+    async editar(req, res) {
+        try {
+            const { local_id } = req.params
+            if (isNaN(Number(local_id))) {
+                return res.status(400).json({ message: 'ID inválido!' });
+            }
+            const dados = req.body
+            const local = await Locais.findOne({
+                where: {
+                    id: local_id,
+                    usuario_id: req.usuario.id
+                },
+                include: {
+                    model: Atividades,
+                    as: 'atividades',
+                    through: {
+                        attributes: []
+                    }
+                }
+            })
+            if (!local) {
+                return res.status(404).json({ message: 'Acesso negado ou local não encontrado.' })
+            }
+            dados.nome ? local.nome = dados.nome : null
+            dados.descricao ? local.descricao = dados.descricao : null
+            if (dados.cep) {
+                const localidade = await buscarCep(dados.cep)
+                const maps_link = linkGoogleMaps(localidade.lat, localidade.lng)
+                local.coordenadas = `${localidade.lat}, ${localidade.lng}`
+                local.localidade = `${localidade.address} ${dados.numero ? `${dados.numero}` : 0}, ${localidade.district} - ${localidade.city}, ${localidade.state}`
+                local.maps_link = maps_link
+            }
+            if (Array.isArray(dados.atividades_id) && dados.atividades_id.length > 0) {
+                const atividades_id = await Atividades.findAll({
+                    where: {
+                        id: dados.atividades_id
+                    }
+                }
+                )
+                const idsExistentes = atividades_id.map(a => a.id)
+                const idsNaoExistentes = dados.atividades_id.filter(id => !idsExistentes.includes(id))
+                if (idsNaoExistentes.length > 0) {
+                    return res.status(400).json({ message: 'Uma ou mais atividades não existem!' })
+                }
+                await local.setAtividades(atividades_id)
+            }
+            await local.save()
+            return res.status(200).json({ message: 'Local editado com sucesso!' })
+        } catch (error) {
+            console.log(error)
+            if (error.name === 'SequelizeUniqueConstraintError') {
+                return res.status(400).json({ message: 'Ja existe um local com esse nome!' })
+            }
+            if (error.name === 'Error') {
+                return res.status(400).json({ message: 'CEP inválido ou não encontrado!' })
+            }
+            return res.status(500).json({ message: 'Não foi possível editar o local' })
         }
     }
 }
